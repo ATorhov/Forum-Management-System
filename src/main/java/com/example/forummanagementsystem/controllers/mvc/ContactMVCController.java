932d5523d3be82e41c923c506b92f4a6380e48d3
User <ivan.belev69@gmail.com>
2023-03-28 20:35:44 +0300

    Added contact view and controller. Working properly.

---
+++ src/main/java/com/example/forummanagementsystem/controllers/mvc/ContactMVCController.java
@@ -0,0 +1,39 @@
+package com.example.forummanagementsystem.controllers.mvc;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+
+@Controller
+@RequestMapping
+public class ContactMVCController {
+
+    @PostMapping("/contact")
+    public String contact(Model model) {
+        return "contact";
+    }
+
+    @GetMapping("/contact")
+    public String contact(HttpSession httpSession) {
+
+        return "contact";
+    }
+
+    @PostMapping("posts/contact")
+    public String contactFromPosts(Model model) {
+        return "redirect:/contact";
+    }
+
+    @GetMapping("posts/contact")
+    public String contactFromPosts(HttpSession httpSession) {
+
+        return "redirect:/contact";
+    }
+}
