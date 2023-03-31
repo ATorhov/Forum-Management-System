dbc73c87bfb631fbcf233f5eae4d1f91688a0c24
Aleks Torhov <arcturus1986@abv.bg>
2023-03-31 14:07:41 +0300

    Swagger dependency and annotation in run added.

--- src/main/java/com/example/forummanagementsystem/ForumManagementSystemApplication.java
+++ src/main/java/com/example/forummanagementsystem/ForumManagementSystemApplication.java
@@ -2,8 +2,11 @@ package com.example.forummanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @SpringBootApplication
+//@EnableWebMvc
 public class ForumManagementSystemApplication {
 
     public static void main(String[] args) {
